4bf2580cc5577f1c6faf4f2e93ed07e0311d8093
Abhijeet Sinha <abhijeetaman007@gmail.com>
2021-09-07 13:15:50 +0530

    Some more minor changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <GlobalProvider>
-      <Header title="Expense Tracker"/>
+      <Header title="Expense Manager"/>
       <div className="container">
         <Balance/>
       </div>
